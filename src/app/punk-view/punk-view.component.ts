662885b9a320d163a4c1e5556b860e512a151cd4
onigiri <onigiri@onigiri.onigiri>
2022-07-12 20:22:07 -0400

    Issue with this searcher, need to do another type of form

--- src/app/punk-view/punk-view.component.ts
+++ src/app/punk-view/punk-view.component.ts
@@ -8,14 +8,6 @@ import { BaseChartDirective } from 'ng2-charts';
 import {ErrorStateMatcher} from "@angular/material/core";
 import {FormControl, FormGroupDirective, NgForm, Validators} from "@angular/forms";
 
-/** Error when invalid control is dirty, touched, or submitted. */
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
-
 @Component({
   selector: 'app-punk-view',
   templateUrl: './punk-view.component.html',
@@ -84,7 +76,6 @@ export class PunkViewComponent implements OnInit {
 
   punkFormControl = new FormControl('', [Validators.required, Validators.pattern("^[0-9]*$"), Validators.minLength(1)]);
 
-  matcher = new MyErrorStateMatcher();
 
   gridByBreakpoint = {
     xl: 8,
